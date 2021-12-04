6dc9edacf0a74c66c5e3e226fa7072e2ec03cdd4
bitue <ashikul.islam_ashik@yahoo.com>
2021-12-04 09:21:54 +0600

    binarySearch

---
+++ data.py
@@ -0,0 +1,27 @@
+import math
+nums =[23,56,89,90,100,675]
+
+def binary_search(arr , target):
+    start = 0
+    end = len(arr) -1
+
+    while start <= end :
+        mid = math.floor((start+end)/2)
+        if arr[mid]> target :
+            end = mid -1
+
+        if arr[mid] < target :
+           start = mid +1
+
+
+        if arr[mid] == target :
+            return  mid
+
+
+    return -1  ;
+
+
+selected = input('enter the target value')
+res = binary_search(nums , int(selected));
+
+print(res)
---
+++ main.py
@@ -0,0 +1,21 @@
+
+friends=['ashik', 'razz','ashik', 'anik', 'sen']
+
+co = friends.count('as')
+nums =[1, 5, 34, 7, 2, 1, 34, 444]
+print(co)
+
+friends.sort();
+nums.sort()
+nums.reverse()
+
+print(friends);
+print(nums)
+
+
+friends2 =friends.copy();
+print(friends2)
+
+
+
+
